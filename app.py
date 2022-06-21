62f60edbef02cbf74b5cc0d4e8d45998a134a935
ondoo <90903756+ondoo@users.noreply.github.com>
2022-06-21 11:11:41 +0900

    공유 기능 추가

--- app.py
+++ app.py
@@ -37,11 +37,6 @@ def home():
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
-@app.route('/')
-def main():
-    # DB에서 저장된 단어 찾아서 HTML에 나타내기
-    word = list(db.words.find({}, {"_id": False}))
-    return render_template("index.html", words=words)
 
 
 
@@ -73,7 +68,10 @@ def register():
 
 @app.route('/cordiary')
 def main():
-    return render_template('main.html')
+    # DB에서 저장된 단어 찾아서 HTML에 나타내기
+    word = list(db.words.find({}, {"_id": False}))
+    return render_template("main.html", words=word)
+
 
 @app.route('/mainprac')
 def mainprac():
